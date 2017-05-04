85d7b57e7402656e03089805d27b3b0fb946db19
Ksenia Lepikhina <kklepi@gmail.com>
2017-05-04 23:08:00 +0000

    Add new file

---
+++ discontinuous_proc.R
@@ -0,0 +1,11 @@
+#Snow Duration and Discontinuous Active Layer Depth Processing
+#Earth Lab - Project Permafrost
+#By: Ksenia Lepikhina and Jeffery Thompson (mentor)
+#Copy Right
+
+library(sp)
+library(rgdal)
+library(raster)
+library(rgeos)
+library(ggplot2)
+library(psych)
